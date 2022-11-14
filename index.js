e9dced1583f54ef9a76412c8e659b170eecdfe30
Hara <harachung90@gmail.com>
2022-11-14 22:33:50 +1100

    Second commit. Learned that React is declarative!

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-ReactDOM.render(<h1>Hi, everyone!</h1>, document.getElementById("root"));
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+/*
+
+imperative way of programming
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
+
+ */
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,
+    document.getElementById("root"));
