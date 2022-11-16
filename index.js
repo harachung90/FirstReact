5d997d1c815de60f623573649dbf33ea1e44bc11
Hara Chung <harachung90@gmail.com>
2022-11-16 20:43:55 +1100

    Fourth commit. Tried setting up React locally

--- index.js
+++ index.js
@@ -19,17 +19,18 @@ document.getElementById("root").append(h1)
 
 const page = (
     <nav>
-        <h1>Welcome to Hara's Website</h1>
+        <h1>Why I love React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+
         </ul>
     </nav>
 )
 
-
-
 ReactDOM.render(
     page,
     document.getElementById("root"));
+
